5a1832dc0f8fd8ec555b32f4d1e380fc231f67e6
Robert Weisser <rweisser114@gmail.com>
2014-07-01 10:16:58 -0400

    Finish sign in

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ group :test do
   # gem 'growl', '1.0.3'
 
   gem 'factory_girl_rails', '4.2.0'
+  gem 'cucumber-rails',     '1.4.0', :require => false
+  gem 'database_cleaner',   github: 'bmabey/database_cleaner'
 end
 
 gem 'pg',           '0.17.1'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)  # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -19,10 +20,10 @@ class UsersController < ApplicationController
 
 private
 
-  def user_params
-    params.require(:user).permit(:name,
-                                 :email,
-                                 :password,
-                                 :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
+                                   :password_confirmation)
+    end
 end
